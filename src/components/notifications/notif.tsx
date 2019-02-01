eead0f6bf010862e7a42324838302b2ad7d032e2
Marc Dwyer <marcdwyer1738@gmail.com>
2019-01-31 21:11:58 -0600

    added notif

--- src/components/notifications/notif.tsx
+++ src/components/notifications/notif.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { LSObj } from '../main/main'
-import _ from 'lodash';
 import './notif_styles.scss'
 interface Props{
     live: LSObj
@@ -8,7 +7,11 @@ interface Props{
 interface State {
     loaded: boolean;
     times: number;
-    difference: Array<string>;
+    difference: Checker[];
+}
+interface Checker {
+    name: string;
+    channelId: string;
 }
 export default class Notifications extends Component <Props, State> {
     state: State = {
@@ -17,33 +20,46 @@ export default class Notifications extends Component <Props, State> {
         difference: []
     }
     componentDidMount() {
-
         setTimeout(() => {
             this.setState({loaded: true})
-      }, 5000)
+      }, 500)
    }
    componentDidUpdate(prevProps: Props) {
-       const { times } = this.state
-       if (prevProps !== this.props) {
+       const { times, loaded } = this.state
+       
+       if (loaded) {
+            setTimeout(() => {
+                this.setState({loaded: false})
+            }, 3000)
+       }
+       if (prevProps.live !== this.props.live) {
             this.setState({times: times + 1})
+            console.log(prevProps.live)
             if (times > 1) {
-                const oldNames: Array<string> = Object.values(prevProps.live).map(stream => stream.name)
-                const newNames: Array<string> = Object.values(this.props.live).map(stream => stream.name)
+                const oldNames: Checker[] = Object.values(prevProps.live).map(stream => {
+                    return {name: stream.name, channelId: stream.channelId}
+                })
+                const newNames: Checker[] = Object.values(this.props.live).map(stream => {
+                   return {name: stream.name, channelId: stream.channelId}
+                })
 
-                const diff = _.difference(newNames, oldNames);
-
-                return diff.length > 0 ? this.setState({difference: diff})
+                const diff = this.difference(newNames, oldNames)
+              //  console.log(diff)
+                if (diff.length > 0) {
+                    return this.setState({difference: diff})
+                } else {
+                    return
+                }
             }
        }
    }
     render() {
         const { times, loaded } = this.state
-        console.log(loaded)
         if (loaded) {
             return (
                 <div className="notification prompt"
                 >
-                hello?
+                <span>I will notify you when streamers go live!</span>
                 </div>
             )
         }
@@ -52,5 +68,21 @@ export default class Notifications extends Component <Props, State> {
             >
             </div>
         )
+    }
+    difference(newNames: Checker[], old: Checker[]): Checker[] {
+        let ch: Checker[] = []
+        for (let x = 0; x < newNames.length; x++) {
+            for (let i = 0; i < old.length; i++) {
+                let match: boolean = false
+                if (newNames[x].name === old[i].name) {
+                    match = true
+                    break
+                }
+                if (i === old.length && !match) {
+                    ch.push(newNames[x])
+                }
+            }
+        }
+        return ch
     }
 }
